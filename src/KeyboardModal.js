b00b69501e3d1471647ab6a4bae89eb789349f0f
Vegard Egeland <vegardegeland@gmail.com>
2021-02-11 16:31:06 +0100

    Add first version of keyboard shortcut modal popup

--- src/KeyboardModal.js
+++ src/KeyboardModal.js
@@ -1,16 +1,117 @@
-import './Modal.css'
+import "./Modal.css";
 
 export default function KeyboardModal(props) {
-  return <div className={`modal ${props.display ? '' : 'hidden'}`}
-              role="dialog"
-              aria-modal="true"
-              onClick={props.onClose}>
-    <section onClick={e => e.stopPropagation()}>
-    <button id="keyboardClose"
-            class="close"
-            aria-label="Close"
-            onClick={props.onClose}>X</button>
-            <h1>Keyboard Shortcuts</h1>
-    </section>
-  </div>
+  return (
+    <div
+      className={`modal ${props.display ? "" : "hidden"}`}
+      role="dialog"
+      aria-modal="true"
+      onClick={props.onClose}
+    >
+      <section onClick={(e) => e.stopPropagation()}>
+        <button
+          id="keyboardClose"
+          class="close"
+          aria-label="Close"
+          onClick={props.onClose}
+        >
+          X
+        </button>
+        <h1>Keyboard Shortcuts</h1>
+        <table>
+          <tbody>
+            <tr>
+              <td align="center">
+                <span class="shortcut">Esc</span>
+              </td>
+              <td>Close modal popup</td>
+            </tr>
+            <tr>
+              <td align="center">
+                <span class="shortcut">Q</span> –{" "}
+                <span class="shortcut">U</span>
+              </td>
+              <td>Press/unpress key in octave 3</td>
+            </tr>
+            <tr>
+              <td align="center">
+                <span class="shortcut">A</span> –{" "}
+                <span class="shortcut">J</span>
+              </td>
+              <td>Press/unpress key in octave 4</td>
+            </tr>
+            <tr>
+              <td align="center">
+                <span class="shortcut">Z</span> –{" "}
+                <span class="shortcut">M</span>
+              </td>
+              <td>Press/unpress key in octave 5</td>
+            </tr>
+            <tr>
+              <td align="center">
+                <span class="shortcut">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="10"
+                    height="10"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M7.27 2.047a1 1 0 0 1 1.46 0l6.345 6.77c.6.638.146 1.683-.73 1.683H11.5v3a1 1 0 0 1-1 1h-5a1 1 0 0 1-1-1v-3H1.654C.78 10.5.326 9.455.924 8.816L7.27 2.047z" />
+                  </svg>{" "}
+                  Shift
+                </span>
+              </td>
+              <td align="center">
+                Increase pressed note (<span class="shortcut">Q</span>{" "}–{" "}
+                <span class="shortcut">M</span>) by a semitone
+              </td>
+            </tr>
+            <tr>
+              <td align="center">
+                <span class="shortcut">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="12"
+                    height="12"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M3.86 8.753l5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z" />
+                  </svg>
+                </span>{" "}
+                <span class="shortcut">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="12"
+                    height="12"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M12.14 8.753l-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
+                  </svg>
+                </span>
+              </td>
+              <td>Transpose pressed keys up/down one semitone</td>
+            </tr>
+            <tr>
+              <td align="center">
+                <span class="shortcut">Tab</span>
+              </td>
+              <td>Navigate between focusable elements</td>
+            </tr>
+            <tr>
+              <td align="center">
+                <span class="shortcut">Enter</span>
+              </td>
+              <td>Click focused element</td>
+            </tr>
+            <tr>
+              <td align="center">
+                <span class="shortcut">Space</span>
+              </td>
+              <td>Reset piano</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+    </div>
+  );
 }
