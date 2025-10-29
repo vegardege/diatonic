ef4ba358eb9490cb12a465251b531684e4782711
Vegard Egeland <vegardegeland@gmail.com>
2025-10-29 14:00:03 +0100

    Move `AppControls` to separate component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { CHORDS, Note, NoteList, SCALES } from "kamasi";
 import { useEffect, useState } from "react";
 
 import styles from "./App.module.css";
+import AppControls from "./components/AppControls/AppControls.tsx";
 import AudioControls from "./components/AudioControls/AudioControls.tsx";
 import FlexControls from "./components/FlexControls/FlexControls.tsx";
 import HelpModal from "./components/HelpModal/HelpModal.tsx";
@@ -11,7 +12,6 @@ import PatternList from "./components/PatternList/PatternList.tsx";
 import RootNote from "./components/RootNote/RootNote.tsx";
 import Search from "./components/Search/Search.tsx";
 import TabControls from "./components/TabControls/TabControls.tsx";
-import ThemeToggle from "./components/ThemeToggle/ThemeToggle.tsx";
 import { useAudioEngine } from "./hooks/useAudioEngine.ts";
 
 // NoteListConstructor type for pattern handlers
@@ -383,61 +383,11 @@ export default function App() {
           onPlayChord={handlePlayChord}
           onPlayScale={handlePlayScale}
         />
-        <div className={styles.navInstruments}></div>
-        <div className={styles.navButtons}>
-          <button
-            className={styles.navButton}
-            type="button"
-            title="Reset"
-            onClick={() => clearPressed()}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              viewBox="-2 -2 18 18"
-            >
-              <title>Reset icon</title>
-              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z" />
-            </svg>
-            Reset
-          </button>
-          <ThemeToggle />
-          <button
-            className={styles.navButton}
-            type="button"
-            title="Keyboard shortcuts"
-            onClick={() => showModal("keyboard", "keyboardClose")}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              viewBox="0 0 16 16"
-            >
-              <title>Keyboard icon</title>
-              <path d="M0 6a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6zm13 .25v.5c0 .138.112.25.25.25h.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25h-.5a.25.25 0 0 0-.25.25zM2.25 8a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5A.25.25 0 0 0 3 8.75v-.5A.25.25 0 0 0 2.75 8h-.5zM4 8.25v.5c0 .138.112.25.25.25h.5A.25.25 0 0 0 5 8.75v-.5A.25.25 0 0 0 4.75 8h-.5a.25.25 0 0 0-.25.25zM6.25 8a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5A.25.25 0 0 0 7 8.75v-.5A.25.25 0 0 0 6.75 8h-.5zM8 8.25v.5c0 .138.112.25.25.25h.5A.25.25 0 0 0 9 8.75v-.5A.25.25 0 0 0 8.75 8h-.5a.25.25 0 0 0-.25.25zM13.25 8a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25h-.5zm0 2a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25h-.5zm-3-2a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h1.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25h-1.5zm.75 2.25v.5c0 .138.112.25.25.25h.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25h-.5a.25.25 0 0 0-.25.25zM11.25 6a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25h-.5zM9 6.25v.5c0 .138.112.25.25.25h.5a.25.25 0 0 0 .25-.25v-.5A.25.25 0 0 0 9.75 6h-.5a.25.25 0 0 0-.25.25zM7.25 6a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5A.25.25 0 0 0 8 6.75v-.5A.25.25 0 0 0 7.75 6h-.5zM5 6.25v.5c0 .138.112.25.25.25h.5A.25.25 0 0 0 6 6.75v-.5A.25.25 0 0 0 5.75 6h-.5a.25.25 0 0 0-.25.25zM2.25 6a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h1.5A.25.25 0 0 0 4 6.75v-.5A.25.25 0 0 0 3.75 6h-1.5zM2 10.25v.5c0 .138.112.25.25.25h.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25h-.5a.25.25 0 0 0-.25.25zM4.25 10a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h5.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25h-5.5z" />
-            </svg>
-            Keys
-          </button>
-          <button
-            className={styles.navButton}
-            type="button"
-            title="Help"
-            onClick={() => showModal("help", "helpClose")}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              viewBox="-2 -2 18 18"
-            >
-              <title>Help icon</title>
-              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.496 6.033h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286a.237.237 0 0 0 .241.247zm2.325 6.443c.61 0 1.029-.394 1.029-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94 0 .533.425.927 1.01.927z" />
-            </svg>
-            Help
-          </button>
-        </div>
+        <AppControls
+          onReset={clearPressed}
+          onShowKeyboardShortcuts={() => showModal("keyboard", "keyboardClose")}
+          onShowHelp={() => showModal("help", "helpClose")}
+        />
       </nav>
       <div className={styles.piano}>
         <Piano
